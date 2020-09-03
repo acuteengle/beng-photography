9bacdcb03c452da7a1c97db4d7e9b18422fb9f4b
Brandon Eng <bce25@cornell.edu>
2020-09-03 12:06:04 -0700

    lazy loading, lozad, spinner

--- gallery.js
+++ gallery.js
@@ -2,16 +2,23 @@ function shuffle(array) {
     array.sort(() => Math.random() - 0.5);
 }
 
+const observer = lozad();
+observer.observe();
+
 $(document).ready(() => {
     shuffle(images);
     for (const name of images) {
         let element = document.createElement("IMG");
         let url = "./images/" + name;
-        element.src = url;
+        element['data-src'] = url;
+        element.class = "lozad"
         element.alt = name;
+        element.loading = "lazy";
         element.setAttribute("data-image", url);
         $("#gallery").append(element);
     }
     $("#gallery").unitegallery();
+    $("#spinner").css("display", "none");
+    $("#gallery").css("display", "block");
     $(".main-content").fadeIn(2000);
 });
